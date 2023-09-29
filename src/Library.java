2749d5da54ad7c2753ed43640a3d0c0537f0758b
Linus Ilbratt <linusilbratt@gmail.com>
2023-09-29 09:13:44 +0200

    Changed the borrowBook method to eliminate the break statement

--- src/Library.java
+++ src/Library.java
@@ -2,23 +2,23 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class Library {
     private List<Book> books; // Creates a list called 'books' that can maintain several objects of the type 'Book'. 'Book' is a blueprint to create more book objects.
 
 
-    public Library(){ // Constructor that creates a new instance of the class when you use 'new SpookyLibrary()'
+    public Library() { // Constructor that creates a new instance of the class when you use 'new SpookyLibrary()'
         this.books = new ArrayList<>(); // Initiate the 'books' variable to a new instance of the class 'ArrayList' 'ArrayList' is an implementation of the interface 'List'
-        books.add(new Book("Haunted Mansion", "Unkown", "Unkown", 1431, true));
+        books.add(new Book("Haunted Mansion", "Unknown", "Unknown", "Unknown", true));
     }
 
     public void addBook(Book book) { // Method declaration. It is public, so it can be called from other classes.
         books.add(book); // Within the method we call the add function
     }
 
-    public void searchBook(String titel){
+    public void searchBook(String titel) {
         for (Book book : books) {
-            if (book.getBookTitel().equalsIgnoreCase(titel))
-            {
+            if (book.getBookTitel().equalsIgnoreCase(titel)) {
                 System.out.println(book.toString());
             }
         }
@@ -33,31 +33,39 @@ public class Library {
     }
 
     public void borrowBook(String title) {
-        boolean bookFound = false;
+
+        Book foundBook = findBookByTitle(title);
+        if (foundBook != null) {
+            // Check if book is available first
+            if (foundBook.isBookAvailable()) {
+                // If the books is "Haunted Mansion" start the game
+                if (title.equalsIgnoreCase("Haunted Mansion")) {
+                    HauntedMansionGame game = new HauntedMansionGame();
+                    game.start(); // Continue with the code after the game is over
+                }
+
+                foundBook.borrowBook();
+                System.out.println("You have borrowed " + title + " enjoy!");
+            } else {
+                System.out.println("The book " + title + " is currently not available");
+            }
+        } else {
+            System.out.println("I'm sorry " + title + " doesn't exist in our library at the moment.");
+        }
+
+    }
+
+    private Book findBookByTitle(String title) {
         for (Book book : books) {
             if (book.getBookTitel().equalsIgnoreCase(title)) {
-                bookFound = true;
-
-                // If the book is "Haunted Mansion", start the game
-                if (title.equalsIgnoreCase("Haunted Mansion")){
-                    HauntedMansionGame game = new HauntedMansionGame();
-                    game.start();
-                    return; // When the game is over, exit the method.
-                }
-                if (book.isBookAvailable()) {
-                    book.borrowBook();
-                    System.out.println("You have borrowed " + title + " enjoy!");
-                } else {
-                    System.out.println("The book " + title + " is currently not available");
-                }
-                break;
+                return book;
             }
         }
-        if (!bookFound) {
-            System.out.println("Im sorry " + title + " doesn't exist in our library at the moment.");
-        }
+        return null;
     }
-    public void returnBook(String titel){
+
+
+    public void returnBook(String titel) {
         for (Book book : books) {
             if (book.getBookTitel().equalsIgnoreCase(titel)) {
                 book.returnBook();
